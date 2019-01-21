20017a0d38559314b81435910192ce629cd3deee
BrotherDonkey <willbjorn@gmail.com>
2019-01-20 19:23:07 -0800

    hide settings until it will do something.

--- ts/settings.ts
+++ ts/settings.ts
@@ -1,4 +1,9 @@
 export function setupSettingsToggle(): void {
+    const settingsButton = document.getElementById('settings-button') as HTMLElement;
+    if (settingsButton) {
+        settingsButton.hidden = false;
+    }
+
     window.addEventListener('click', (e: Event) => {
         const target = e.target as HTMLElement;
         if (target && (target.id === 'settings-button' || target.closest('#settings-button'))) {
