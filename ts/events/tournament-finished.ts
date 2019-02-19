28f1a8eea2c1d3ffab2264f8d88bda1e6b61986b
BrotherDonkey <willbjorn@gmail.com>
2019-02-18 16:56:06 -0800

    proc congratulations

--- ts/events/tournament-finished.ts
+++ ts/events/tournament-finished.ts
@@ -5,10 +5,21 @@ export function setupTournamentFinishedEvent() {
         const finishedGame = e.detail as IGame;
         console.log(finishedGame);
         console.log('We are done here.');
+        const overlay = document.getElementById('tournament-finished-animated-overlay') as HTMLElement;
+        renderTournamentResults(overlay, finishedGame);
     });
 }
 
-export function renderTournamentResults(container: HTMLElement): HTMLElement {
+export function renderTournamentResults(container: HTMLElement, game: IGame): HTMLElement {
+    const winnerNameElt = container.querySelector('.congrats-winner-name') as HTMLElement;
+    winnerNameElt.textContent = game.winner.name;
+
+    container.hidden = false;
+    const star = container.querySelector('.fas.fa-star') as HTMLSpanElement;
+    const winnerElt = container.querySelector('.congrats-winner') as HTMLSpanElement;
+
+    star.classList.add('pop-once');
+    winnerElt.classList.add('appear-from-bottom')
 
     return container;
 }
